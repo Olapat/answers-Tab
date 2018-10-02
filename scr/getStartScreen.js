e9cc2bb647b4813be073585f36480150b1fe0bcb
Olapat <lapatbr19@gmail.com>
2018-10-02 21:35:49 +0700

    Revert "update display show Data Score"
    This reverts commit e168c6e89e4b2e2e0fcbdfa7b22bb9ca0f3f985e.

--- scr/getStartScreen.js
+++ scr/getStartScreen.js
@@ -1,104 +1,35 @@
-import React, { PureComponent } from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, View, TouchableHighlight, Text, Image } from 'react-native';
 import { Container } from 'native-base';
-import Modal from 'react-native-modalbox';
 
-export default class homeScreen extends PureComponent {
+export default class homeScreen extends Component {
     static navigationOptions = {
         header: null
     };
 
-    constructor() {
-        super();
-        this.state = {
-            onOpen: false,
-            Data: true
-        }
-    };
-
-    _onOpenModal = () => {
-        this.setState({onOpen : true});
-    };
-
-    _onCloseModal = () => {
-        this.setState({onOpen : false,  Data: false});
-    };
-
     render() {
-        let DataScore = this.props.navigation.getParam('DataScore');
-        let newDataScore;
-        DataScore ? newDataScore = DataScore : null;
-        if (DataScore && this.state.Data) {
-            this.setState({onOpen: true});
-        }
-
+        let ScoreTotal = this.props.navigation.getParam('ScoreTotal');
         return(
             <Container style={styles.container}>
-                {newDataScore ?
-                <Modal
-                    style={{backgroundColor: '#ad7a56', height: "40%", borderWidth: 10, borderColor: '#502701',}}
-                    isOpen={this.state.onOpen}
-                    // backdropPressToClose={false}
-                    onClosed={this._onCloseModal}
-                >
-                    <View style={[styles.viewScoreTotal,{alignSelf: 'center', marginTop: '2%'}]}>
-                        <View style={styles.viewDataScore}>
-                            <Text style={styles.texts}>{`Score: `}</Text>
-                            {/*<Text style={styles.texts}>{"140"}</Text>*/}
-                            <Text style={styles.texts}>{newDataScore.Score}</Text>
-                        </View>
-                        <View style={styles.viewDataScore}>
-                            <Text style={styles.texts}>{`Time Left: `}</Text>
-                            {/*<Text style={styles.texts}>{'26'}</Text>*/}
-                            <Text style={styles.texts}>{newDataScore.TimeLeftScore}</Text>
-                        </View>
-                        <View style={styles.viewDataScore}>
-                            <Text style={styles.texts}>{`HP Left: `}</Text>
-                            <Text style={styles.texts}>{newDataScore.HpLeftScore}</Text>
-                            {/*<Text style={styles.texts}>{'6'}</Text>*/}
-                        </View>
-                        <View style={[styles.viewDataScore,{marginTop: '5%'}]}>
-                            <Text style={styles.text}>{`ScoreTotal:`}</Text>
-                            {/*<Text style={styles.text}>{`140`}</Text>*/}
-                            <Text style={styles.text}>{newDataScore.ScoreTotal}</Text>
-                        </View>
-                        <TouchableHighlight
-                            style={styles.touchM}
-                            onPress={this._onCloseModal}
-                        >
-                            <Text style={[styles.textM,{marginTop: '3%'}]}>{"OK"}</Text>
-                        </TouchableHighlight>
-                    </View>
-                </Modal>
-                    : null
-                }
                 <Text style={styles.textTitleQuiz}>{"Asnwers Tab"}</Text>
                 <Image
                     style={styles.image}
                     source={require('../public/img/quiz.png')}
                 />
-                <TouchableHighlight
-                    style={styles.touchPlay}
-                    onPress={() => this.props.navigation.navigate('GamePlayScreen')}
-                >
-                    <Text style={[styles.text,{marginTop: '3%'}]}>{"Start Game"}</Text>
-                </TouchableHighlight>
-                {newDataScore ?
+                <View style={styles.viewCenter}>
                     <TouchableHighlight
-                        style={styles.viewScoreTotalTouch}
-                        onPress={this._onOpenModal}
-                        underlayColor={'#813101'}
+                        style={styles.touchPlay}
+                        onPress={() => this.props.navigation.navigate('GamePlayScreen')}
                     >
-                        <View>
-                            <View style={[styles.viewDataScoreT,{marginTop: '5%'}]}>
-                                <Text style={styles.text}>{`ScoreTotal:`}</Text>
-                                {/*<Text style={styles.text}>{`140`}</Text>*/}
-                                <Text style={styles.text}>{newDataScore.ScoreTotal}</Text>
-                            </View>
-                        </View>
+                        <Text style={styles.text}>{"Start Game"}</Text>
                     </TouchableHighlight>
-                    : null
-                }
+                    <View>
+                        {ScoreTotal || ScoreTotal === 0 ?
+                            <Text style={styles.text}>{`Score: ${ScoreTotal}`}</Text>
+                            : null
+                        }
+                    </View>
+                </View>
             </Container>
         );
     }
@@ -108,29 +39,20 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
         backgroundColor: '#ad7a56',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     image: {
-        width: '62%',
-        height: '41%',
-        marginVertical: '10%',
+        width: 200,
+        height: 200,
+        margin: '10%',
+        alignSelf: 'center'
+    },
+    viewCenter: {
+        alignItems: 'center'
     },
     touchPlay: {
         backgroundColor: '#502701',
-        // paddingHorizontal: '10%',
-        width: '70%',
-        height: '10%',
-        alignItems: 'center',
-        borderWidth: 2,
-        borderRadius: 20,
-        borderColor: '#000',
-        marginBottom: '5%',
-    },
-    touchM: {
-        backgroundColor: '#502701',
-        width: '70%',
-        height: '20%',
-        alignItems: 'center',
+        paddingHorizontal: '10%',
         borderWidth: 2,
         borderRadius: 20,
         borderColor: '#000',
@@ -141,44 +63,13 @@ const styles = StyleSheet.create({
         fontSize: 36,
         fontWeight: 'bold',
         color: '#502701',
-        marginTop: '10%',
+        marginTop: '15%',
+        marginBottom: '5%',
     },
     text: {
         fontSize: 36,
         fontWeight: 'bold',
         color: '#fff',
     },
-    textM: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-    viewScoreTotal: {
-        alignItems: 'center',
-        width: '70%',
-    },
-    viewScoreTotalTouch: {
-        alignItems: 'center',
-        width: '80%',
-        borderWidth: 5,
-        borderRadius: 20,
-        borderColor: '#502701',
-        justifyContent: 'center',
-    },
-    texts: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-    viewDataScore: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-    },
-    viewDataScoreT: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '90%',
-    }
 });
 
